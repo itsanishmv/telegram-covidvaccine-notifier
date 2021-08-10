2b90270035ca8020f89fc37c523407d66d97a0ac
anish <aanishmv@gmail.com>
2021-08-10 23:52:15 +0530

    netlify commit 2

--- index.js
+++ index.js
@@ -17,29 +17,31 @@ var cowinApi = async () => {
     
     
 }
-function getData(item){
+function getData(item) {
     console.log(item)
     if (item.available_capacity_dose1 === 0) {
         passMessage("testing netlify server 3.0.4")
       
     } else {
         if (item.fee_type === "free") {
-            passMessage(`Sir, your covid vax slot is Available at  ${item.block_name} ${ item.name } vaccine : ${item.vaccine} fee : ${item.fee}Rs`)
+            passMessage(`Sir, your covid vax slot is Available at  ${item.block_name} ${item.name} vaccine : ${item.vaccine} fee : ${item.fee}Rs`)
             
         } else {
             console.log(' Only paid vaccine slots')
         }
   
+    }
 }
 
-setInterval(() => {
-    cowinApi()
-},70000)
+    setInterval(() => {
+        cowinApi()
+    }, 70000)
 
 
-function passMessage(message) {
-    var chatId = "democovid"
-    var tel_api = `https://api.telegram.org/bot1875198205:AAHwFMtztN8qx2Gi1okDIoKS2PCJuySEVt4/sendMessage?chat_id=@${chatId}&text=${message}`
-       req(tel_api)
+    function passMessage(message) {
+        var chatId = "democovid"
+        var tel_api = `https://api.telegram.org/bot1875198205:AAHwFMtztN8qx2Gi1okDIoKS2PCJuySEVt4/sendMessage?chat_id=@${chatId}&text=${message}`
+        req(tel_api)
     
-}
+    }
+
